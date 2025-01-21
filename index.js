395168381c45882cfe51d7e663074ffe50c29f93
shivamshrivaastava <shivamshrivaastava@gmail.com>
2025-01-21 19:45:56 +0530

    js

---
+++ index.js
@@ -0,0 +1,42 @@
+// console.log("1");
+
+
+function first() {
+    console.log("A");
+    second()
+    console.log("B")
+    
+    
+}
+
+
+function second() {
+    console.log("c");
+    third()
+    console.log("D");
+    
+}
+
+
+function third () {
+    console.log("6");
+    console.log("7");
+    
+    
+}
+
+first()
+
+console.log("8");
+
+
+// console.log("1");
+// setTimeout(function setTimeout(){
+//     console.log(2)
+    
+// },4000);
+
+console.log(3)
+
+
+
